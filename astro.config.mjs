787f65a62bfa26846e40beafb024f34d39115760
Deny Ary Septian Lazuardi <denyaryseptianl@gmail.com>
2026-03-24 19:50:14 +0700

    ganti hybyrd server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid', // Allows API routes to work
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [
     react(),
     tailwind()
